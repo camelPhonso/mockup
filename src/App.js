60e0eee99ecd144f4f15752a468e3f4776a8bdec
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-06-25 17:49:13 +0100

    defined changeHandler to pass to second Dropdown rendered on the page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import "./App.css";
-import Dropdown from "./components/dropdown";
-import getAllRoutes from "./utils/getAllRoutes";
-import { useEffect, useState } from "react";
+import "./App.css"
+import Dropdown from "./components/dropdown"
+import getAllRoutes from "./utils/getAllRoutes"
+import { useEffect, useState } from "react"
 
 function App() {
-  const [allRoutes, setAllRoutes] = useState([]);
-  const [selectedRoute, setSelectedRoute] = useState('');
-  const [selectedRouteWithId, setSelectedRouteWithId] = useState('');
+  const [allRoutes, setAllRoutes] = useState([])
+  const [selectedRoute, setSelectedRoute] = useState("")
+  const [selectedRouteWithId, setSelectedRouteWithId] = useState("")
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const enablingHandler = () => {
+    setIsDisabled(false)
+  }
 
   useEffect(() => {
     const fetch = async () => {
-      const fetchedRoutes = await getAllRoutes();
-      setAllRoutes(fetchedRoutes);
-    };
+      const fetchedRoutes = await getAllRoutes()
+      setAllRoutes(fetchedRoutes)
+    }
 
-    fetch();
-  }, []);
+    fetch()
+  }, [])
 
   return (
     <div className="App">
       <h1>Your Component should be showing on this page!!!</h1>
-      <Dropdown 
-        routes={allRoutes} 
-        selectedRoute={selectedRoute} 
-        setSelectedRoute={setSelectedRoute} 
-        showId={false} 
+      <Dropdown
+        routes={allRoutes}
+        selectedRoute={selectedRoute}
+        setSelectedRoute={setSelectedRoute}
+        changeHandler={enablingHandler}
+        showId={false}
+        isDisabled={false}
       />
       {selectedRoute && <p>You selected: {selectedRoute}</p>}
-      
-      <Dropdown 
-        routes={allRoutes} 
-        selectedRoute={selectedRouteWithId} 
-        setSelectedRoute={setSelectedRouteWithId} 
-        showId={true} 
+
+      <Dropdown
+        routes={allRoutes}
+        selectedRoute={selectedRouteWithId}
+        setSelectedRoute={setSelectedRouteWithId}
+        showId={true}
+        isDisabled={isDisabled}
       />
       {selectedRouteWithId && <p>You selected: {selectedRouteWithId}</p>}
     </div>
-  );
+  )
 }
 
-export default App;
-
-
+export default App

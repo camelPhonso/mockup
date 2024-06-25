4428e90b593ccb13dee6db9ccf06da720690cdd0
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-06-25 17:18:25 +0100

    added isDisabled prop to control dropdown component

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React from "react"
 
-export default function Dropdown({ routes, selectedRoute, setSelectedRoute, showId }) {
+export default function Dropdown({
+  routes,
+  selectedRoute,
+  setSelectedRoute,
+  showId,
+  isDisabled,
+}) {
   function handleChange(event) {
-    setSelectedRoute(event.target.value);
+    setSelectedRoute(event.target.value)
   }
 
   // Ensure routes are defined before accessing them
   if (!routes || routes.length === 0) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
     <div>
-      <select id="dropdown" value={selectedRoute} onChange={handleChange}>
+      <select
+        id="dropdown"
+        value={selectedRoute}
+        onChange={handleChange}
+        disabled={isDisabled}
+      >
         <option value="">--Please choose an option--</option>
         {routes.map((route, index) => (
-          <option key={index} value={route.name}>
+          <option
+            key={index}
+            value={route.name}
+          >
             {route.name} {showId && `(${route.routeId})`}
           </option>
         ))}
       </select>
     </div>
-  );
+  )
 }
